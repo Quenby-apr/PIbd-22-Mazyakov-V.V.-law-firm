e260ed9d3205225b3f0cf684e6ac558783cc9383
влад мазяков <mazyakov02@mail.ru>
2021-03-21 13:56:59 +0400

    .

--- LawFirm/LawFirmFileImplement/Implements/DocumentStorage.cs
+++ LawFirm/LawFirmFileImplement/Implements/DocumentStorage.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using LawFirmBusinessLogic.BindingModels;
 using LawFirmBusinessLogic.Interfaces;
 using LawFirmBusinessLogic.ViewModels;
--- LawFirm/LawFirmFileImplement/Implements/OrderStorage.cs
+++ LawFirm/LawFirmFileImplement/Implements/OrderStorage.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using LawFirmBusinessLogic.BindingModels;
 using LawFirmBusinessLogic.Interfaces;
 using LawFirmBusinessLogic.ViewModels;
