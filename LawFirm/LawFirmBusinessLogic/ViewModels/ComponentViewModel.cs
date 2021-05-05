d13573dc1ebb60ab8c717e5dc515ee787d6a92de
влад мазяков <mazyakov02@mail.ru>
2021-05-05 11:13:26 +0400

    /

--- LawFirm/LawFirmBusinessLogic/ViewModels/ComponentViewModel.cs
+++ LawFirm/LawFirmBusinessLogic/ViewModels/ComponentViewModel.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace LawFirmBusinessLogic.ViewModels
@@ -8,10 +9,13 @@ namespace LawFirmBusinessLogic.ViewModels
     /// <summary> 
     ///  Компонент, требуемый для изготовления изделия  
     ///  </summary>
+    [DataContract]
     public class ComponentViewModel
     {
+        [DataMember]
         public int Id { get; set; }
         [DisplayName("Название компонента")]
+        [DataMember]
         public string ComponentName { get; set; }
     }
 }
--- LawFirm/LawFirmBusinessLogic/ViewModels/WarehouseViewModel.cs
+++ LawFirm/LawFirmBusinessLogic/ViewModels/WarehouseViewModel.cs
@@ -1,23 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Runtime.Serialization;
 using System.Text;
 
 namespace LawFirmBusinessLogic.ViewModels
 {
+    [DataContract]
     public class WarehouseViewModel
     {
+        [DataMember]
         public int Id { get; set; }
 
         [DisplayName("Название склада")]
+        [DataMember]
         public string WarehouseName { get; set; }
 
         [DisplayName("ФИО ответственного")]
+        [DataMember]
         public string NameResponsiblePerson { get; set; }
 
         [DisplayName("Дата создания")]
+        [DataMember]
         public DateTime DateCreate { get; set; }
-
+        [DataMember]
         public Dictionary<int, (string, int)> WarehouseComponents { get; set; }
     }
 }
